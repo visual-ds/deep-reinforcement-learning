f13796f0e8933580e9e928a5d0dca862aa6c1514
Lucas Emanuel Resck Domingues <41991486+lucasresck@users.noreply.github.com>
2020-01-27 20:12:51 -0300

    Finish initial version

--- Deep Reinforcement Learning/deep_q_learning.py
+++ Deep Reinforcement Learning/deep_q_learning.py
@@ -1,118 +1,143 @@
 from collections import deque
 from keras.layers import Dense
-from keras.models import Sequential
+from keras.models import load_model, Sequential
 from keras.optimizers import Adam
+import argparse
 import gym
 import numpy as np
 import os
 import pickle
 import random
+import time
 
 
 class NeuralNetwork():
-    def __init__(self, obs_size, action_size):
-        self.obs_size = obs_size
+    def __init__(self, obs_shape, action_size, learning_rate=0.001):
+        self.obs_shape = obs_shape
         self.action_size = action_size
-        self.learning_rate = 0.001
-        self.model_of_network()
+        self.learning_rate = learning_rate
+        self.model = self.model_of_network()
 
     def model_of_network(self):
-        self.model = Sequential()
-        self.model.add(Dense(units=24, input_shape=(self.obs_size,), activation='relu'))
-        self.model.add(Dense(units=48, activation='relu'))
-        self.model.add(Dense(units=self.action_size, activation='linear'))
-        self.model.compile(loss='mse', optimizer=Adam(lr=self.learning_rate))
+        model = Sequential()
+        model.add(Dense(units=24, input_shape=self.obs_shape, activation='relu'))
+        model.add(Dense(units=48, activation='relu'))
+        model.add(Dense(units=self.action_size, activation='linear'))
+        model.compile(loss='mse', optimizer=Adam(lr=self.learning_rate))
+        return model
 
 
 class Agent():
-    def __init__(self, env_name):
+    def __init__(
+        self,
+        env_name='MountainCar-v0',
+        gamma=0.999,
+        n_episodes=500,
+        max_iterations=200,
+        epsilon_decay=0.05,
+        epsilon_min=0.01,
+        replay_memory_capacity=20000,
+        minibatch_size=32
+        ):
         self.env = gym.make(env_name)
-        self.env.seed(0)
-        np.random.seed(0)    
-        self.gamma = 1.0
-        self.n_episodes = 500
-        self.max_iterations = 200
-        self.obs_size = self.env.env.observation_space.shape[0]
+        self.set_seeds(int(time.time()))
+        self.gamma = gamma
+        self.n_episodes = n_episodes
+        self.max_iterations = max_iterations
+        self.obs_shape = self.env.observation_space.shape
         self.action_size = self.env.env.action_space.n
         self.epsilon = 1.0
-        self.epsilon_decay = 0.05
-        self.epsilon_min = 0.01
-        self.epsilon_max_iterations = 1000
-        self.replay_memory_capacity = 20000
-        self.n_epochs = 100
-        self.minibatch_size = 32
+        self.epsilon_decay = epsilon_decay
+        self.epsilon_min = epsilon_min
+        self.replay_memory_capacity = replay_memory_capacity
+        self.minibatch_size = minibatch_size
+        self.neural_network = NeuralNetwork(self.obs_shape, self.action_size)
 
-    def read_policy(self, policy_path):
-        '''
-        If there's already a policy, read it.
-        '''
-
-        with open(policy_path, 'rb') as f:
-            self.policy = pickle.load(f)
+    def set_seeds(self, seed):
+        """Set random seeds using current time."""
+        self.env.seed(seed)
+        np.random.seed(seed)
+        random.seed(seed)
 
     def train(self):
-        '''
-        Train deep Q-learning agent.
-        '''
-
+        """Train deep Q-learning agent."""
         self.deep_q_learning()
 
     def deep_q_learning(self):
-        '''
-        Deep Q-learning.
+        """
+        Deep Q-learning algorithm.
         It learns the Q function without knowing the transition probabilities, through deep neural networks.
-        '''
-
+        """
         self.replay_memory = deque(maxlen=self.replay_memory_capacity)
-        self.neural_network = NeuralNetwork(self.obs_size, self.action_size)
+        self.target_network = NeuralNetwork(self.obs_shape, self.action_size)
+        self.target_network.model.set_weights(self.neural_network.model.get_weights())
 
         for episode in range(self.n_episodes):
             self.train_episode(episode)
+            if (episode + 1) % 50 == 0:
+                self.sample(1)
 
         self.env.close()
 
     def train_episode(self, episode):
+        """Train one episode of deep Q-learning."""
         obs = self.env.reset()
-        state = obs#self.normalize_obs(obs)
+        state = obs  # self.normalize_obs(obs)
         total_reward = 0
         for i in range(self.max_iterations):
             action = self.take_action(state)
             obs, reward, done, _ = self.env.step(action)
-            state_ = obs#self.normalize_obs(obs)
+            state_ = obs  # self.normalize_obs(obs)
             self.replay_memory.append([state, action, reward, state_, done])
             self.train_from_replay()
-            # experience = self.sample_experience()
-            # y = self.set_target(experience, done)
-            # self.update_network(y, experience)
             state = state_
             total_reward += reward
             if done:
                 break
-        result = 'FAIL'
-        if i < 199:
-            result = 'SUCESS'
-        print('Ep. {}: {}. Reward = {} and epsilon = {}.'.format(episode, result, total_reward, self.epsilon), end='\n\n')
-        self.epsilon -= self.epsilon_decay
-        self.epsilon = np.max([self.epsilon, self.epsilon_min])
+        self.report(i, episode, total_reward)
+        self.update_epsilon()
+        self.sync_networks()
+            
+    def take_action(self, state):
+        """
+        Take action based in epsilon-greedy algorithm.
+        With small probabily, take a random action;
+        otherwise, take the action from the neural network.
+        """
+        if np.random.rand() < self.epsilon:
+            action = np.random.choice(self.action_size)
+        else:
+            output = self.compute_values(state)
+            action = np.argmax(output)
+        return action
+
+    def compute_values(self, state):
+        """Compute values of a state."""
+        logits = self.neural_network.model.predict(state[None, :])
+        return logits[0, :]
 
     def train_from_replay(self):
+        """Train neural network from samples of replay memory."""
         if len(self.replay_memory) < self.minibatch_size:
+            # If there isn't enough samples
             return
         minibatch = random.sample(self.replay_memory, self.minibatch_size)
         states = []
-        targets = []
+        states_ = []
         for sample in minibatch:
             state, action, reward, state_, done = sample
             states.append(state)
-            target = self.compute_values(state)
-            if done:
-                target[action] = reward
-            else:
-                target[action] = reward + self.gamma*np.max(self.compute_values(state_))
-            targets.append(target)
-
+            states_.append(state_)
         states = np.array(states)
-        targets = np.array(targets)
+        states_ = np.array(states_)
+        targets = self.neural_network.model.predict(states)
+        targets_ = self.target_network.model.predict(states_)
+        for i, sample in enumerate(minibatch):
+            state, action, reward, state_, done = sample
+            if done:
+                targets[i][action] = reward
+            else:
+                targets[i][action] = reward + self.gamma * np.max(targets_[i])
 
         self.neural_network.model.fit(
             x=states,
@@ -120,7 +145,23 @@ class Agent():
             verbose=0
         )
 
-            
+    def report(self, i, episode, total_reward):
+        """Show status on console."""
+        result = 'FAIL'
+        if i < self.max_iterations - 1:
+            result = 'SUCCESS'
+        print('Ep. {}: {}. Reward = {} and epsilon = {}.'.format(episode, result, total_reward, self.epsilon), end='\n\n')
+
+    def update_epsilon(self):
+        """Update epsilon for epsilon-greedy algorithm."""
+        if self.epsilon > self.epsilon_min:
+            self.epsilon -= self.epsilon_decay
+            self.epsilon = np.max([self.epsilon, self.epsilon_min])
+
+    def sync_networks(self):
+        """Sync original and target neural networks."""
+        self.target_network.model.set_weights(self.neural_network.model.get_weights())
+
     def normalize_obs(self, obs):
         '''
         Normalize observation.
@@ -129,81 +170,35 @@ class Agent():
 
         state = (obs - self.env.env.low) / (self.env.env.high - self.env.env.low)
         return state
-            
-    def take_action(self, state):
-        '''
-        Take action based in epsilon greedy algorithm.
-        With small probabily, take a random action;
-        otherwise, take the action from the neural network.
-        '''
-        if np.random.rand() < self.epsilon:
-            action = np.random.choice(self.env.action_space.n)
-        else:
-            output = self.compute_values(state)
-            action = np.argmax(output)
-        return action
 
     def sample_experience(self):
         """Sample experience from replay memory."""
         return self.replay_memory[np.random.choice(len(self.replay_memory))]
 
-    def set_target(self, experience, done):
-        """Set target for neural network update."""
-        state, action, reward, state_ = experience
-        if done:
-            return reward
-        max_Q = np.max(self.compute_values(state_))
-        return reward + self.gamma*max_Q
-
-    def update_network(self, y, experience):
-        """Update neural network from replay_memory."""
-        state, action, reward, state_ = experience
-        input_ = state[None, :]
-        target = self.neural_network.model.predict(input_)
-        target[0, action] = y
-        self.neural_network.model.fit(input_, target, verbose=0, epochs=self.n_epochs)
-
-    def compute_values(self, state):
-        '''
-        Compute values of a state.
-        '''
-
-        logits = self.neural_network.model.predict(state[None, :])
-        return logits[0, :]
-
     def save_network(self, network_path):
-        '''
-        Save the network in order to run it faster.
-        '''
+        """Save the network in order to run it faster."""
+        self.neural_network.model.save(network_path)
+        print('Neural network saved.', end='\n\n')
 
-        with open(network_path, 'wb') as f:
-            pickle.dump(self.neural_network.model, f, pickle.HIGHEST_PROTOCOL)
+    def load_network(self, network_path):
+        """Load the network in order to run it faster."""
+        self.neural_network.model = load_model(network_path)
+        print('Neural network loaded.', end='\n\n')
 
-    def read_network(self, network_path):
-        '''
-        Read the network in order to run it faster.
-        '''
-        self.neural_network.model
-        with open(network_path, 'rb') as f:
-            self.neural_network.model = pickle.load(f)
-
-    def sample(self):
-        '''
-        It shows a sample of our policy.
-        '''
-
-        for _ in range(5):
-            _ = self.run_episode(render=True)
+    def sample(self, n):
+        """Sample the network."""
+        print('Sampling network:')
+        for _ in range(n):
+            self.run_episode(render=True)
+        print()
 
     def run_episode(self, render=False):
-        '''
-        It runs one episode for our policy.
-        '''
-
+        """Run one episode for our network."""
         obs = self.env.reset()
         total_reward = 0
         for _ in range(self.max_iterations):
-            state = self.normalize_obs(obs)
+            # state = self.normalize_obs(obs)
+            state = obs
             action = self.which_action(state)
             obs, reward, done, _ = self.env.step(action)
             total_reward += reward
@@ -211,34 +206,32 @@ class Agent():
                 self.env.render()
             if done:
                 break
-
         self.env.close()
-        return total_reward
+        print('Reward: ', total_reward)
 
     def which_action(self, state):
+        """Select which is the best action based on the network."""
         return np.argmax(self.compute_values(state))
 
-    def test(self):
-        scores = [self.run_episode() for _ in range(10)]
-        print('Average total reward: {}.'.format(np.mean(scores)))
+    # def test(self):
+    #     """Test neural network."""
+    #     scores = [self.run_episode() for _ in range(10)]
+    #     print('Average total reward: {}.'.format(np.mean(scores)))
 
 
-def main():
-    
-    import argparse
+def main():    
     parser = argparse.ArgumentParser()
     parser.add_argument('--run', action='store_true')
     args = parser.parse_args()
 
-    agent = Agent('MountainCar-v0')
-
+    agent = Agent()
     if args.run:
-        agent.read_network('deep_q_learning.pkl')
-        agent.sample()
+        agent.load_network('deep_q_learning.h5')
+        agent.sample(5)
     else:
         agent.train()
-        agent.save_network('deep_q_learning.pkl')
-    agent.test()
+        agent.save_network('deep_q_learning.h5')
+    # agent.test()
 
 
 if __name__ == '__main__':
