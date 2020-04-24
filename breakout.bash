29c0ae46200236b3dd3f73c75a6fab98b6f65bd3
Lucas Emanuel Resck Domingues <41991486+lucasresck@users.noreply.github.com>
2020-04-23 22:28:52 -0300

    Update file

--- breakout.bash
+++ breakout.bash
@@ -1,2 +1,2 @@
 docker run --gpus all -it --rm -v $PWD:/tmp -w /tmp my-tensorflow-gpu-py3 python -m cProfile -o stats/local-gpu-docker ./breakout_deep_q_learning.py --gpu
-# docker run --gpus all -it -p 8888:8888 my-tensorflow-gpu-py3-jupyter
+# docker run --gpus all -it --rm -v $(realpath ~/GitHub):/tf/notebooks -p 8888:8888 tensorflow/tensorflow:latest-gpu-py3-jupyter
