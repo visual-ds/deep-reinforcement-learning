1066272fcd349fd327d04f9b36c740bbd3bfded3
Lucas Emanuel Resck Domingues <41991486+lucasresck@users.noreply.github.com>
2020-04-23 22:28:16 -0300

    Add a lot of optimizations

--- breakout_deep_q_learning.py
+++ breakout_deep_q_learning.py
@@ -1,6 +1,5 @@
 # Inspired by 'Playing Atari with Deep Reinforcement Learning' (https://arxiv.org/abs/1312.5602)
 
-from __future__ import absolute_import, division, print_function, unicode_literals
 from collections import deque
 # from gym import wrappers
 import argparse
@@ -14,7 +13,7 @@ import time
 
 
 class NeuralNetwork():
-    def __init__(self, input_shape, action_size, learning_rate=0.001, summary=False):
+    def __init__(self, input_shape, action_size, learning_rate=0.00025, summary=False):
         self.input_shape = input_shape
         self.action_size = action_size
         self.learning_rate = learning_rate
@@ -24,24 +23,25 @@ class NeuralNetwork():
     def model_of_network(self):
         model = tf.keras.Sequential([
             tf.keras.layers.Conv2D(
+                input_shape=self.input_shape,
                 filters=16,
                 kernel_size=8,
-                input_shape=self.input_shape
+                strides=(4, 4),
+                activation='relu'
             ),
-            tf.keras.layers.MaxPooling2D(),
             tf.keras.layers.Conv2D(
-                filters=16,
-                kernel_size=4
+                filters=32,
+                kernel_size=4,
+                strides=(2, 2),
+                activation='relu'
             ),
-            tf.keras.layers.MaxPooling2D(),
             tf.keras.layers.Flatten(),
             tf.keras.layers.Dense(
-                units=128,
+                units=256,
                 activation='relu'
             ),
             tf.keras.layers.Dense(
-                units=self.action_size,
-                activation='linear'
+                units=self.action_size
             )
         ])
         if self.summary:
@@ -49,21 +49,60 @@ class NeuralNetwork():
         model.compile(loss='mean_squared_error', optimizer=tf.keras.optimizers.RMSprop(learning_rate=self.learning_rate))
         return model
 
+class ReplayMemory():
+    def __init__(self, maxlen, minibatch_size, n_channels):
+        self.replay_memory = deque(maxlen=maxlen)
+        self.minibatch_size = minibatch_size
+        self.n_channels = n_channels
+
+    def append(self, experience):
+        """Apend experience to replay memory."""
+        self.replay_memory.append(experience)
+
+    def last_4_states(self, state):
+        """Concatenate last four states."""
+        state = state[..., np.newaxis]  # Actual state
+        for i in range(3):  # Last three states in replay memory
+            state = np.concatenate((state, self.replay_memory[-i-1][0][..., np.newaxis]), axis=2)        
+        return state / 255
+
+    def minibacth(self):
+        """Sample a minibatch from the memory replay."""
+        # Choose self.minibatch_size indices from replay memory
+        i_minibatch = random.sample(range(3, len(self.replay_memory) - 1), self.minibatch_size)
+        # Get these samples
+        minibatch = [self.replay_memory[i].copy() for i in i_minibatch]
+        # Fill up the states and states_
+        my_minibatch = minibatch.copy()
+        for i, sample in enumerate(minibatch):
+            state = self.full_state(i_minibatch[i])
+            my_minibatch[i][0] = state
+            state_ = self.full_state(i_minibatch[i]+1)
+            my_minibatch[i].insert(3, state_)
+        minibatch = my_minibatch
+        return minibatch
+
+    def full_state(self, i):
+        """Get the full state of the i sample."""
+        state = self.replay_memory[i][0][..., np.newaxis]
+        for j in range(3):
+            state = np.concatenate((state, self.replay_memory[i-j-1][0][..., np.newaxis]), axis=2)
+        return state / 255
 
 class DeepQAgent():
     def __init__(
         self,
         record,
         env_name='BreakoutDeterministic-v4',
-        gamma=0.999,
-        max_frames=1000000,
+        gamma=0.99,
+        max_frames=10000000,
         max_iterations=1000000,
-        epsilon_decay_until=100000,
+        epsilon_decay_until=10000000,
         epsilon_min=0.1,
-        replay_memory_capacity=100000,
+        replay_memory_capacity=1000000,
+        start_replay=50000,
         minibatch_size=32,
-        nn_input_shape=(80, 80, 4),
-        n_history = 4
+        nn_input_shape=(105, 80, 4),
     ):
         self.env = gym.make(env_name)
         if record:
@@ -78,11 +117,17 @@ class DeepQAgent():
         self.epsilon_decay_until = epsilon_decay_until
         self.epsilon_min = epsilon_min
         self.replay_memory_capacity = replay_memory_capacity
+        # self.replay_memory = deque(maxlen=self.replay_memory_capacity)
+        self.start_replay = start_replay
         self.minibatch_size = minibatch_size
         self.nn_input_shape = nn_input_shape
         self.neural_network = NeuralNetwork(self.nn_input_shape, self.action_size, summary=True)
-        self.n_history = n_history
-        self.history = deque(maxlen=self.n_history)
+        self.i_frames = 0
+        self.replay_memory = ReplayMemory(
+            maxlen=self.replay_memory_capacity,
+            minibatch_size=self.minibatch_size,
+            n_channels = self.nn_input_shape[2]
+        )
 
     def set_seeds(self, seed):
         """Set random seeds using current time."""
@@ -93,19 +138,57 @@ class DeepQAgent():
     def train(self):
         """Train deep Q-learning agent."""
         self.start_time = time.time()
+        self.fill_replay_memory()
         self.deep_q_learning()
+
+    def fill_replay_memory(self):
+        """Fill replay memory on start."""
+
+        print('\nFilling replay memory...', end='')
+        while self.i_frames < self.start_replay:
+            obs = self.env.reset()
+            self.state = self.preprocess(obs)
+            for _ in range(self.max_iterations):
+                action = self.env.action_space.sample()
+                obs, reward, done, _ = self.env.step(action)
+                reward = self.transform_reward(reward)
+                self.replay_memory.append([self.state, action, reward, done])
+                self.state = self.preprocess(obs)
+                self.i_frames += 1
+                if not self.i_frames < self.start_replay:
+                    break
+                elif done:
+                    break
+        print(' Done.')
+
+    def preprocess(self, obs):
+        """Preprocess observation."""
+        img = self.resize(obs)
+        img = self.to_gray_scale(img)
+        # img = self.cut(gray)
+        # state = self.create_state(img[..., np.newaxis])
+        return img.astype(np.uint8)
+    
+    def resize(self, img):
+        img = img[::2, ::2]
+        return img
+
+    def to_gray_scale(self, img):
+        return np.mean(img, axis=2)     
+
+    def transform_reward(self, reward):
+        return np.sign(reward)
 
     def deep_q_learning(self):
         """
         Deep Q-learning algorithm.
         It learns the Q function without knowing the transition probabilities, through deep neural networks.
         """
-        self.replay_memory = deque(maxlen=self.replay_memory_capacity)
+        # Copy network
         self.target_network = NeuralNetwork(self.nn_input_shape, self.action_size)
         self.target_network.model.set_weights(self.neural_network.model.get_weights())
 
-        episode = 0
-        self.i_frames = 0
+        episode = 0  # Flag
         while self.i_frames < self.max_frames:
             if self.train_episode(episode):
                 break
@@ -117,60 +200,38 @@ class DeepQAgent():
 
     def train_episode(self, episode):
         """Train one episode of deep Q-learning."""
+        # Flags
         self.time_flag = time.time()
         self.frames_flag = self.i_frames
+        max_frames = False
 
-        self.history = deque(maxlen=self.n_history)
+        total_reward = 0  # Cumulative
+
         obs = self.env.reset()
-        state = self.preprocess(obs)
-        total_reward = 0
+        self.state = self.preprocess(obs)
+
         for i in range(self.max_iterations):
-            action = self.take_action(state)
+            action = self.take_action()
             obs, reward, done, _ = self.env.step(action)
-            state_ = self.preprocess(obs)
-            self.replay_memory.append([state, action, reward, state_, done])
-            self.train_from_replay()
-            state = state_
-            total_reward += reward
+            reward = self.transform_reward(reward)
+            self.replay_memory.append([self.state, action, reward, done])
             self.i_frames += 1
-            # self.env.render()
-            if self.i_frames >= self.max_frames:  # Max frames
-                return True
-            if done:
+            total_reward += reward
+
+            self.state = self.preprocess(obs)
+            self.train_from_replay()
+            if not self.i_frames < self.max_frames:  # Max frames
+                max_frames = True
+                break
+            elif done:
                 break
         self.report(i, episode, total_reward)
         self.sync_networks()
-        return False
+        if (episode + 1) % 100 == 0:
+            self.save_network('models/deep_q_learning-{}.h5'.format(episode))
+        return max_frames
 
-    def preprocess(self, obs):
-        """Preprocess observation."""
-        gray = self.gray(obs)
-        img = self.cut(gray)
-        img = self.resize(img)
-        state = self.create_state(img)
-        return state
-
-    def gray(self, obs):
-        return np.mean(obs, axis=2)
-
-    def cut(self, gray):
-        img = gray[32:, :]
-        x, y = img.shape
-        img = img[:-np.abs(x-y), :]
-        return img
-    
-    def resize(self, img):
-        img = img[list(range(0, img.shape[0], 2)), :][:, list(range(0, img.shape[1], 2))]
-        return img.reshape(img.shape[0], img.shape[1], 1)
-
-    def create_state(self, img):
-        self.history.append(img)
-        state = np.concatenate(tuple(self.history), axis=2)
-        for _ in range(self.n_history - state.shape[2]):
-            state = np.concatenate((state, self.history[-1]), axis=2)  
-        return state
-
-    def take_action(self, state):
+    def take_action(self):
         """
         Take action based in epsilon-greedy algorithm.
         With small probabily, take a random action;
@@ -180,7 +241,11 @@ class DeepQAgent():
         if np.random.rand() < self.epsilon:
             action = np.random.choice(self.action_size)
         else:
-            output = self.compute_values(state)
+            last_4_states = self.replay_memory.last_4_states(self.state)
+        #     state = self.replay_memory[-1][0][..., np.newaxis]
+        #     for i in range(1, 4):
+        #         state = np.concatenate((state, self.replay_memory[-1-i][0][..., np.newaxis]), axis=2)
+            output = self.compute_values(last_4_states)
             action = np.argmax(output)
         return action
 
@@ -189,17 +254,37 @@ class DeepQAgent():
         epsilon = 1 + (self.i_frames * self.epsilon_min - self.i_frames) / self.epsilon_decay_until
         self.epsilon = np.max([self.epsilon_min, epsilon])
 
-    def compute_values(self, state):
+    def compute_values(self, last_4_states):
         """Compute values of a state."""
-        logits = self.neural_network.model.predict(state[None, :])
-        return logits[0, :]
+        logits = self.neural_network.model.predict(last_4_states[np.newaxis, ...])
+        return logits[0]
+
+    # def train_from_replay(self):
+    #     minibatch = self.replay_memory.minibatch()
+    #     states = []
+    #     actions = []
+    #     rewards = []
+    #     states_ = []
+    #     dones = []
+    #     for sample in minibatch:
+    #         state, action, reward, state_, done = sample
+    #         states.append(state)
+    #         actions.append(action)
+    #         rewards.append(reward)
+    #         states_.append(state_)
+    #         dones.append(done)
+    #     targets = self.neural_network.model.predict(states)
+    #     targets_ = self.target_network.model.predict(states_)
+    #     for i, target in enumerate(targets):
+    #         if dones[i]:
+    #             targets[i][actions[i]] = rewards[i]
+    #         else:
+    #             targets[i][actions[i]] = rewards[i] + self.gamma * np.max(targets_[i])
+    #     self.neural_network.model.fit(states, targets)
 
     def train_from_replay(self):
         """Train neural network from samples of replay memory."""
-        if len(self.replay_memory) < self.minibatch_size:
-            # If there isn't enough samples
-            return
-        minibatch = random.sample(self.replay_memory, self.minibatch_size)
+        minibatch = self.replay_memory.minibacth()
         states, states_ = self.extract_states(minibatch)
         targets = self.calculate_targets(minibatch, states, states_)
         self.fit(states, targets)
@@ -208,7 +293,7 @@ class DeepQAgent():
         states = []
         states_ = []
         for sample in minibatch:
-            state, action, reward, state_, done = sample
+            state, _, _, state_, _ = sample
             states.append(state)
             states_.append(state_)
         states = np.array(states)
@@ -218,7 +303,7 @@ class DeepQAgent():
     def calculate_targets(self, minibatch, states, states_):
         targets, targets_ = self.target_predict(states, states_)
         for i, sample in enumerate(minibatch):
-            state, action, reward, state_, done = sample
+            _, action, reward, _, done = sample
             if done:
                 targets[i][action] = reward
             else:
@@ -270,10 +355,6 @@ class DeepQAgent():
         """Sync original and target neural networks."""
         self.target_network.model.set_weights(self.neural_network.model.get_weights())
 
-    def sample_experience(self):
-        """Sample experience from replay memory."""
-        return self.replay_memory[np.random.choice(len(self.replay_memory))]
-
     def save_network(self, network_path):
         """Save the network in order to run it faster."""
         os.makedirs(os.path.dirname(network_path), exist_ok=True)
@@ -285,32 +366,32 @@ class DeepQAgent():
         self.neural_network.model = tf.keras.models.load_model(network_path)
         print('Neural network loaded.', end='\n\n')
 
-    def sample(self, n=5):
-        """Sample the network."""
-        print('Sampling network:')
-        for _ in range(n):
-            self.run_episode(render=True)
-        print()
+    # def sample(self, n=5):
+    #     """Sample the network."""
+    #     print('Sampling network:')
+    #     for _ in range(n):
+    #         self.run_episode(render=True)
+    #     print()
 
-    def run_episode(self, render=False):
-        """Run one episode for our network."""
-        obs = self.env.reset()
-        total_reward = 0
-        for _ in range(self.max_iterations):
-            state = self.preprocess(obs)
-            action = self.which_action(state)
-            obs, reward, done, _ = self.env.step(action)
-            total_reward += reward
-            if render:
-                self.env.render()
-            if done:
-                break
-        self.env.close()
-        print('Reward: ', total_reward)
+    # def run_episode(self, render=False):
+    #     """Run one episode for our network."""
+    #     obs = self.env.reset()
+    #     total_reward = 0
+    #     for _ in range(self.max_iterations):
+    #         state = self.preprocess(obs)
+    #         action = self.which_action(state)
+    #         obs, reward, done, _ = self.env.step(action)
+    #         total_reward += reward
+    #         if render:
+    #             self.env.render()
+    #         if done:
+    #             break
+    #     self.env.close()
+    #     print('Reward: ', total_reward)
 
-    def which_action(self, state):
-        """Select which is the best action based on the network."""
-        return np.argmax(self.compute_values(state))
+    # def which_action(self, state):
+    #     """Select which is the best action based on the network."""
+    #     return np.argmax(self.compute_values(state))
 
 def gpu_setup():
     """Config GPU for TensorFlow."""
@@ -337,11 +418,11 @@ def main():
         gpu_setup()
     agent = DeepQAgent(False)
     if args.run:
-        agent.load_network('data/deep_q_learning.h5')
+        agent.load_network('models/deep_q_learning.h5')
         agent.sample()
     else:
         agent.train()
-        agent.save_network('data/deep_q_learning.h5')
+        agent.save_network('models/deep_q_learning.h5')
 
 if __name__ == '__main__':
     main()
